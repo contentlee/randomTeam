fcddd86c1fc6dbafae6fbd85166f5a94ed48a03e
contentguy <gogumag3@naver.com>
2023-08-08 16:56:55 +0900

    fix: inputArray split 수정

--- src/utils/uploadFile.ts
+++ src/utils/uploadFile.ts
@@ -1,3 +1,7 @@
 export const initialInput = (ls: string) => ls.split(":")[1].split(",");
 
-export const intialInputArray = (ls: string) => initialInput(ls).map((arr) => arr.replace(/[\[,\]]/g, "").split(","));
+export const intialInputArray = (ls: string) =>
+  ls
+    .split(":")[1]
+    .split("],[")
+    .map((arr) => [...arr.replace(/[\[\]]/g, "").split(",")]);
